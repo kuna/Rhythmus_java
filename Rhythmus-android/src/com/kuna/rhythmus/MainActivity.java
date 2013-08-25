f6af69e8baa4b7e8fc2fa1d152ae37afb4742c9a
kuna <kgdgwn98@gmail.com>
2013-08-25 12:58:40 +0900

    screen won't turn off

--- Rhythmus-android/src/com/kuna/rhythmus/MainActivity.java
+++ Rhythmus-android/src/com/kuna/rhythmus/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kuna.rhythmus;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -10,6 +11,9 @@ public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Dont let screen off
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useGL20 = true;	// original: false
