591173041172b185987d3a8acfcf81334118269d
kuna <kgdgwn98@gmail.com>
2013-08-26 12:16:03 +0900

    fixed comparsion error (rarely occured)

--- Rhythmus/src/com/kuna/rhythmus/BMSKeyData.java
+++ Rhythmus/src/com/kuna/rhythmus/BMSKeyData.java
@@ -13,12 +13,7 @@ public class BMSKeyData implements Comparable<BMSKeyData> {
 			return -1;
 		else if (this.beat > o.beat)
 			return 1;
-		else {
-			// if same? then BPM should be last argument
-			if (o.key == 3 || o.key == 8)
-				return 1;
-			else 
-				return -1;
-		}
+		else
+			return 0;
 	}
 }
